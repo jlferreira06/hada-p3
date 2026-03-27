ddcd6a395de7669186035c1b1db039f780a10134
adri <adrimelenchon@gmail.com>
2026-03-27 21:27:13 +0100

    55019952L

--- library/ENCategory.cs
+++ library/ENCategory.cs
@@ -9,24 +9,6 @@ namespace library
     public class ENCategory
     {
 
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-        // Constructor vacío
-        public ENCategory() { }
-
-        // Método que pide la práctica: leer todas las categorías
-        public List<ENCategory> ReadAll()
-        {
-            CADCategory cad = new CADCategory();
-            return cad.ReadAll();
-        }
-
-        // Método que pide la práctica: leer una sola
-        public bool Read(ENCategory en)
-        {
-            CADCategory cad = new CADCategory();
-            return cad.Read(this);
-        }
+       
     }
 }
