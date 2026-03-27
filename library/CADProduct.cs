3595bcbfd7a34077bb5203909ac4b79109b847a4
adri <adrimelenchon@gmail.com>
2026-03-27 17:02:21 +0100

    55019952L CADProducts.cs

--- library/CADProduct.cs
+++ library/CADProduct.cs
@@ -26,9 +26,187 @@ namespace library
             SqlConnection con = new SqlConnection(constring);
             try
             {
-                
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                string sql = "INSERT INTO Products (code, name, amount, price, category, creationDate) " +
+                             "VALUES ('" + en.Code + "', '" + en.Name + "', " + en.Amount + ", " +
+                             en.Price + ", " + en.Category + ", '" + en.CreationDate.ToString("yyyy-MM-dd HH:mm:ss") + "')";
+                SqlCommand com = new SqlCommand(sql, c);
+                com.ExecuteNonQuery();
+                c.Close();
+            }
+            catch (SqlException ex)
+            {
+                respuesta = false;
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
             }
 
-            return true;
+            return respuesta;
         }
+
+
+        public bool update(ENProduct en)
+        {
+            bool ok = true;
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                string sql = "UPDATE Products SET name='" + en.Name + "', amount=" + en.Amount +
+                             ", price=" + en.Price + ", category=" + en.Category +
+                             " WHERE code='" + en.Code + "'";
+                SqlCommand com = new SqlCommand(sql, c);
+                com.ExecuteNonQuery();
+                c.Close();
+            }
+            catch (SqlException ex)
+            {
+                ok = false;
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            return ok;
+        }
+
+        public bool delete(ENProduct en)
+        {
+            bool ok = true;
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                string sql = "DELETE FROM Products WHERE code='" + en.Code + "'";
+                SqlCommand com = new SqlCommand(sql, c);
+                com.ExecuteNonQuery();
+                c.Close();
+            }
+            catch (SqlException ex)
+            {
+                ok = false;
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            return ok;
+        }
+
+        public bool read(ENProduct en)
+        {
+            bool ok = false;
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                string sql = "SELECT * FROM Products WHERE code='" + en.Code + "'";
+                SqlCommand com = new SqlCommand(sql, c);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.Name = dr["name"].ToString();
+                    en.Amount = int.Parse(dr["amount"].ToString());
+                    en.Price = float.Parse(dr["price"].ToString());
+                    en.Category = int.Parse(dr["category"].ToString());
+                    en.CreationDate = DateTime.Parse(dr["creationDate"].ToString());
+                    ok = true;
+                }
+                c.Close();
+            }
+            catch (SqlException ex)
+            {
+                ok = false;
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            return ok;
+        }
+
+
+        public bool readFirst(ENProduct en)
+        {
+            bool ok = false;
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                string sql = "SELECT TOP 1 * FROM Products ORDER BY code ASC";
+                SqlCommand com = new SqlCommand(sql, c);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.Code = dr["code"].ToString();
+                    en.Name = dr["name"].ToString();
+                    en.Amount = int.Parse(dr["amount"].ToString());
+                    en.Price = float.Parse(dr["price"].ToString());
+                    en.Category = int.Parse(dr["category"].ToString());
+                    en.CreationDate = DateTime.Parse(dr["creationDate"].ToString());
+                    ok = true;
+                }
+                c.Close();
+            }
+            catch (SqlException ex)
+            {
+                ok = false;
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            return ok;
+        }
+
+        public bool readNext(ENProduct en)
+        {
+            bool ok = false;
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                string sql = "SELECT TOP 1 * FROM Products WHERE code > '" + en.Code + "' ORDER BY code ASC";
+                SqlCommand com = new SqlCommand(sql, c);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.Code = dr["code"].ToString();
+                    en.Name = dr["name"].ToString();
+                    en.Amount = int.Parse(dr["amount"].ToString());
+                    en.Price = float.Parse(dr["price"].ToString());
+                    en.Category = int.Parse(dr["category"].ToString());
+                    en.CreationDate = DateTime.Parse(dr["creationDate"].ToString());
+                    ok = true;
+                }
+                c.Close();
+            }
+            catch (SqlException ex)
+            {
+                ok = false;
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            return ok;
+        }
+
+        public bool readPrev(ENProduct en)
+        {
+            bool ok = false;
+            try
+            {
+                SqlConnection c = new SqlConnection(constring);
+                c.Open();
+                string sql = "SELECT TOP 1 * FROM Products WHERE code < '" + en.Code + "' ORDER BY code DESC";
+                SqlCommand com = new SqlCommand(sql, c);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.Code = dr["code"].ToString();
+                    en.Name = dr["name"].ToString();
+                    en.Amount = int.Parse(dr["amount"].ToString());
+                    en.Price = float.Parse(dr["price"].ToString());
+                    en.Category = int.Parse(dr["category"].ToString());
+                    en.CreationDate = DateTime.Parse(dr["creationDate"].ToString());
+                    ok = true;
+                }
+                c.Close();
+            }
+            catch (SqlException ex)
+            {
+                ok = false;
+                Console.WriteLine("Product operation has failed. Error: {0}", ex.Message);
+            }
+            return ok;
+        }
+
+
+    }
 }
