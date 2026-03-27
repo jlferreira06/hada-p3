4886ff7046c396a844d85711971f5525f52bc75a
adri <adrimelenchon@gmail.com>
2026-03-27 22:17:17 +0100

    55019952L errores

--- library/CADCategory.cs
+++ library/CADCategory.cs
@@ -9,6 +9,50 @@ namespace library
 {
     public class CADCategory
     {
-       
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True";
+
+        public CADCategory() { }
+
+        public List<ENCategory> ReadAll()
+        {
+            List<ENCategory> lista = new List<ENCategory>();
+            SqlConnection c = new SqlConnection(connectionString);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("SELECT * FROM Categories", c);
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    ENCategory cat = new ENCategory();
+                    cat.Id = int.Parse(dr["id"].ToString());
+                    cat.Name = dr["name"].ToString();
+                    lista.Add(cat);
+                }
+            }
+            catch (Exception ex) { }
+            finally { c.Close(); }
+            return lista;
+        }
+
+        public bool Read(ENCategory en)
+        {
+            bool ok = false;
+            SqlConnection c = new SqlConnection(connectionString);
+            try
+            {
+                c.Open();
+                SqlCommand com = new SqlCommand("SELECT * FROM Categories WHERE id = " + en.Id, c);
+                SqlDataReader dr = com.ExecuteReader();
+                if (dr.Read())
+                {
+                    en.Name = dr["name"].ToString();
+                    ok = true;
+                }
+            }
+            catch (Exception ex) { ok = false; }
+            finally { c.Close(); }
+            return ok;
+        }
     }
 }
--- library/ENCategory.cs
+++ library/ENCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,22 @@ namespace library
 {
     public class ENCategory
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
 
-       
+        public ENCategory() { }
+
+        public List<ENCategory> ReadAll()
+        {
+            CADCategory cad = new CADCategory();
+            return cad.ReadAll();
+        }
+
+        public bool Read(ENCategory en)
+        {
+            CADCategory cad = new CADCategory();
+            return cad.Read(this);
+        }
+
     }
 }
